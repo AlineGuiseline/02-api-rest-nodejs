c809cc50031454625a4b00136aa13c2fb72f4020
Aline Guiseline <alineguiseline@gmail.com>
2025-02-07 17:23:57 -0300

    fix: creates amount migration

---
+++ db/migrations/20250207202128_rename_amout_to_amount.ts
@@ -0,0 +1,15 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.alterTable('transactions', function (table) {
+        table.renameColumn('amout', 'amount');
+    });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.alterTable('transactions', function (table) {
+        table.renameColumn('amount', 'amout'); // Volta para "amout" se precisar reverter
+    });
+}
