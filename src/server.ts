22ff98d97ff65932325f228e567eccc66ab4d97a
Aline Guiseline <alineguiseline@gmail.com>
2025-02-07 16:57:40 -0300

    fix: render adjustments

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { env } from "./env"
 app.listen({
     // faz a aplicação ouvir uma porta (precisa ser um objeto)
     port: env.PORT,
+    host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
 }).then(() => {
     console.log('HTTP Server Running')
     // o listen retorna uma promise, então eu quero saber quando a promise
